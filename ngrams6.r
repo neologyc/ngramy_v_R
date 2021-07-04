6a3f2b48e3aabcfe8ebb7f38ce8f395607eddd6a
Neologyc <jaroslav@hlavinka.cz>
2021-07-04 22:54:24 +0200

    Create ngrams6.r

---
+++ ngrams6.r
@@ -0,0 +1,21 @@
+library(quanteda)
+require(readtext)
+
+# načtení souboru
+descs <- read.csv("./data/descs.csv", encoding = "UTF-8")
+# helper, ať vím, co je v datech
+names(descs)
+# nějaký ohák, aby mi to neházelo nějakou chybu
+descs$descs <- as.character(descs$descs)
+# vytvoření korpusu
+corpus_descs <- corpus(descs, text_field = "descs")
+# helper, ať vím, co je v datech
+print(corpus_descs)
+# vytvoření tokenů z corpusu
+tokens_descs <- tokens(corpus_descs)
+
+
+ngrams <- tokens_ngrams(tokens_descs, n = 2:3)
+head(ngrams[[1]], 30)
+
+  
